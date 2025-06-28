3daa6c6e452d0a7b00845f4afdd1f9cefee9d0a4
ruben-nino <ruben.ginebri@gmail.com>
2025-06-28 11:06:40 +0200

    Changed mesh generation to only consider half the wing, renamed some variables to improve readability

--- My_VLM_Implementation/main.py
+++ My_VLM_Implementation/main.py
@@ -4,30 +4,41 @@ from numpy import pi
 
 from functions import *
 
-
+visualize = 0
 
 aero_data, _ = load_aero_data()
+chord           = aero_data["c"][0]
+semi_span       = aero_data['s']
 
 # %% Generate meshes
+
+# number of panels for half the wing (we're using symmetry)
 n_v = 16
 m_v = 10
 c_w_factor = 10 # to multiply by c to obtain the wake length * aero_data['c'][0]
 
-x_points = np.linspace(0, aero_data["c"][0], m_v+1)
-y_points = np.cos( pi - (np.arange( n_v + 2) - 1) * pi / n_v) * aero_data['s']
+x_points = np.linspace(0, chord, m_v + 1)
+y_points = np.cos( pi/2 - np.arange( n_v + 1)  * pi/2 / n_v) * semi_span # only mesh half the wing (use symmetry wrt zx plane)
 
 # geometric mesh
-x_mesh_g, y_mesh_g = np.meshgrid(x_points, y_points)
-# plt.plot(x_mesh_g, y_mesh_g, '*r')
-# plt.xlim(- 0.1, aero_data["s"]/2)
-# plt.show()
+x_mesh_g, y_mesh_g = np.meshgrid(x_points, y_points, indexing='ij')
+
 
 # bound mesh
-delta_x = aero_data["c"][0] / m_v
+delta_x = chord / m_v
 x_mesh_b = x_mesh_g + delta_x / 4
 
 # wake mesh
 x_points_wake = np.linspace(0, c_w_factor * aero_data['c'][0], c_w_factor * m_v + 1 )
-x_points_wake += aero_data["c"][0] + delta_x / 4
+x_points_wake += chord + delta_x / 4
 x_mesh_w, y_mesh_w = np.meshgrid(x_points_wake, y_points)
 
+
+if __name__ == "__main__" and visualize:
+    plt.plot(x_mesh_g, y_mesh_g, '*r')
+    plt.plot(x_mesh_b, y_mesh_g, 'ob', markersize=3)
+    plt.plot(x_mesh_w, y_mesh_w, '1g')
+    plt.xlim(- 0.1, semi_span)
+    plt.hlines(y = semi_span, xmin= - 0.1, xmax=semi_span)
+    plt.show()
+    breakpoint()
